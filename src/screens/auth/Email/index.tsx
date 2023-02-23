fba900e186e0fe700b773168553ea7d1a9870f93
rayanabdirahman <rayanmabdirahman@gmail.com>
2023-02-23 22:19:44 +0000

    add boilerplate UI for Auth email and Sign in password screens

--- src/screens/auth/Email/index.tsx
+++ src/screens/auth/Email/index.tsx
@@ -1,21 +1,35 @@
 import React from "react";
-import { Text } from "react-native";
+import { Stack } from "native-base";
 import Button from "../../../components/atoms/Button";
 import AuthLayout from "../../../components/templates/AuthLayout";
 import {
   AuthScreenProps,
   AuthStackScreenNames,
 } from "../../../navigation/AuthNavigator/types";
+import { Heading, Text } from "../../../components/atoms/Typography";
+import Input from "../../../components/atoms/Input";
 
 type Props = AuthScreenProps<AuthStackScreenNames.EMAIL>;
 
-const EmailScreen: React.FC<Props> = ({ navigation }) => (
-  <AuthLayout>
-    <Text>Email</Text>
-    <Button onPress={() => navigation.navigate("SignInPassword")}>
-      Continue
-    </Button>
-  </AuthLayout>
-);
+const EmailScreen: React.FC<Props> = ({ navigation }) => {
+  return (
+    <AuthLayout>
+      <Stack space={10} pt={4}>
+        <Stack space={8}>
+          <Stack space={1}>
+            <Heading size="xl">Enter your email</Heading>
+            <Text styleType="secondary">
+              Get started by entering your email address
+            </Text>
+          </Stack>
+          <Input label="Email" autoFocus={true} />
+        </Stack>
+        <Button onPress={() => navigation.navigate("SignInPassword")}>
+          Continue
+        </Button>
+      </Stack>
+    </AuthLayout>
+  );
+};
 
 export default EmailScreen;
--- src/screens/auth/signin/Password/index.tsx
+++ src/screens/auth/signin/Password/index.tsx
@@ -1,6 +1,38 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useEffect, useRef } from "react";
+import { Stack } from "native-base";
+import Button from "../../../../components/atoms/Button";
+import { Heading, Text } from "../../../../components/atoms/Typography";
+import AuthLayout from "../../../../components/templates/AuthLayout";
+import {
+  AuthScreenProps,
+  AuthStackScreenNames,
+} from "../../../../navigation/AuthNavigator/types";
+import Input from "../../../../components/atoms/Input";
 
-const PasswordScreen = () => <Text>PasswordScreen</Text>;
+type Props = AuthScreenProps<AuthStackScreenNames.SIGN_IN_PASSWORD>;
+
+const PasswordScreen: React.FC<Props> = ({ navigation }) => {
+  return (
+    <AuthLayout>
+      <Stack space={10} pt={4}>
+        <Stack space={8}>
+          <Stack space={1}>
+            <Heading size="xl">Enter your password</Heading>
+            <Text styleType="secondary">
+              Enter the password you signed up with to continue
+            </Text>
+          </Stack>
+          <Input label="Password" autoFocus={true} />
+        </Stack>
+        <Stack space={1}>
+          <Button onPress={() => navigation.navigate("SignInPassword")}>
+            Sign in
+          </Button>
+          <Text textAlign="center">Forgot password?</Text>
+        </Stack>
+      </Stack>
+    </AuthLayout>
+  );
+};
 
 export default PasswordScreen;
