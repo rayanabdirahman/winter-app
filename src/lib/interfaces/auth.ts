3286526c6f17d7c3ef0486b4f37f57d6a195fc06
rayanabdirahman <rayanmabdirahman@gmail.com>
2023-02-27 21:49:14 +0000

    add auth interfaces

---
+++ src/lib/interfaces/auth.ts
@@ -0,0 +1,7 @@
+export type SignUpConfrmModel = { email: string };
+
+export type SignUpPasswordModel = { password: string };
+
+export type SignUpNameModel = { firstName: string; lastName: string };
+
+export type SignUpUsernameModel = { username: string };
